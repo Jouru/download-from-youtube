910f6380730f1c1648f2e24c285fef31060a42e2
Jouru <104511948+Jouru@users.noreply.github.com>
2023-05-31 15:39:25 +0200

    Add files via upload

--- yt.py
+++ yt.py
@@ -1,64 +1,31 @@
-import tempfile
 import argparse
-import os
-from pytube import Playlist, YouTube
-from pydub import AudioSegment
-
+from ytdl_func import ytdl
 
 def main():
     parser = argparse.ArgumentParser(description="Youtube download script")
     parser.add_argument('--list', help='Playlist options', action='store_true')
-    parser.add_argument('-p', help='Download a youtube Playlist')
+    parser.add_argument('playlist',nargs='?', help='Download all the videos of a youtube Playlist')
     parser.add_argument('-a', help='Download the audio tracks from a playlist')
-    parser.add_argument('-v', help='Download a youtube video')
+    parser.add_argument('video', nargs='?', help='Download a youtube video')
     args = parser.parse_args()
 
     dp = "/home/abner/Downloads/yt-downloads"
 
     if args.list:
         if args.a:
-            download_playlist_audio_only(args.a, dp)
-            print('Download completed!')
-        elif args.p:
-            download_playlist(args.p, dp)
-            print('Done!')
-    elif args.v:
-        download_video(args.v, dp)
-        print('Done!')
+            ytdl.download_playlist_audio_only(args.a, dp)
+            print('\nDownload completed!')
+        else:
+            ytdl.download_playlist(args.playlist, dp)
+            print('\nDone!')
+
+    elif args.a:
+        ytdl.download_audio(args.a, dp)
+        print('\nDone!')
+
     else:
-        download_video(args, dp)
-
-
-def download_playlist(pl, dp):
-    p = Playlist(pl)
-    print(f'Downloading Playlist: {p.title}')
-    playlist_dir = f'{dp}/{p.title}'
-    os.mkdir(playlist_dir)
-    for video in p.videos:
-        video = video.streams.get_highest_resolution()
-        video.download(playlist_dir)
-
-
-def download_playlist_audio_only(pl, dp):
-    p = Playlist(pl)
-    playlist_dir = f'{dp}/{p.title}'
-    os.mkdir(playlist_dir)
-    print(f'Downloading audio playlist: {p.title}')
-    with tempfile.TemporaryDirectory(dir=dp) as tempdir:
-        for track in p.videos:
-            audio = track.streams.get_audio_only()
-            print(f'Downloading {audio.title}')
-            audio.download(tempdir)
-            print(f'Done!\nConverting {audio.title} to flac file')
-            new_audio = AudioSegment.from_file(f'{tempdir}/{audio.title}.mp4')
-            new_audio.export(f'{playlist_dir}/{track.title}.flac', format='flac')
-            print('Done!')
-
-
-def download_video(link, dp):
-    vid = YouTube(link).streams.get_highest_resolution()
-    print(f'Downloading: {vid.title}')
-    vid.download(dp)
+        ytdl.download_video(args.video, dp)
+        print('\nDone!')
 
 
 if __name__=="__main__":
---
+++ ytdl_func.py
@@ -0,0 +1,64 @@
+import tempfile
+import os
+from pytube import Playlist, YouTube
+from pytube.cli import on_progress
+from pydub import AudioSegment
+
+class ytdl:
+    @staticmethod
+    def download_playlist(pl, dp):
+        """Download all the videos in a playlist from youtube"""
+        # Create a Playlist object and create a directory named as the playlist's name
+        p = Playlist(pl)
+        playlist_dir = f'{dp}/{p.title.replace("/", "")}'
+        os.mkdir(playlist_dir)
+        print(f'Downloading Playlist: {p.title}, {p.length} videos', end='\r')
+
+        # Download every single file from the playlist
+        for video in p.video_urls:
+            ytdl.download_video(video, playlist_dir)
+
+    @staticmethod
+    def download_video(url, dp):
+        """Download a video from youtube"""
+        vid = YouTube(url, on_progress_callback=on_progress).streams.get_highest_resolution()
+        print(f'\nDownloading: {vid.title} {vid.filesize_mb} MB')
+        vid.download(dp)
+
+
+    @staticmethod
+    def download_playlist_audio_only(pl, dp):
+        """Download all the audio tracks in a playlist from youtube"""
+        # Create a Playlist object and create a directory named as the playlist's name
+        p = Playlist(pl)
+        playlist_dir = f'{dp}/{p.title.replace("/", "")}'
+        os.mkdir(playlist_dir)
+        print(f'Downloading audio playlist: {p.title}, {p.length} tracks', end='\r')
+
+        # Download every single track from the playlist
+        for track in p.video_urls:
+            ytdl.download_audio(track, playlist_dir)
+
+    @staticmethod
+    def download_audio(url, output_path):
+        """Download the audio from a youtube video"""
+        # Create a youtube object and download it as mp4 file
+        audio = YouTube(url, on_progress_callback=on_progress).streams.get_audio_only()
+        print(f'\nDownloading: {audio.title} {audio.filesize_mb} MB')
+        name = audio.title.replace("/","")
+        audio.download(output_path,filename=name)
+
+        # Convert the mp4 file to a flac file
+        AudioSegment.from_file(f'{output_path}/{name}').export(f'{output_path}/{name}.flac', format='flac')
+        os.remove(f'{output_path}/{name}')
+
+
+    @staticmethod
+    def on_progress(stream, chunk, bytes_remaining):
+        """Callback function"""
+        total_size = stream.filesize
+        bytes_downloaded = total_size - bytes_remaining
+        pct_completed = bytes_downloaded / total_size * 100
+        print(f"\nStatus: {round(pct_completed, 2)} %")
+
+
